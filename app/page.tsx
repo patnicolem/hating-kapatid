32f561df04c9b198393fc5e18d6b68a28525b9a2
Patricia Nicole Malubago <patmalubago@gmail.com>
2026-08-05 22:01:53 +0800

    feat: implement Add User functionality with React State

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,35 @@
-import UserCard from "@/components/UserCard";
-
-const users = [
-  {
-    name: "Pat",
-    balance: 500,
-  },
-  {
-    name: "Alex",
-    balance: -250,
-  },
-  {
-    name: "Carla",
-    balance: 0,
-  },
-];
-
+"use client";
+import { useState } from "react";
 
 
 export default function Home() {
+
+    function addUser() {
+      
+      setUsers([
+        ...users,
+        {
+          name: newUserName,
+          balance: newUserBalance
+        }
+      ]);
+
+      setNewUserName("");
+      setNewUserBalance(0);
+
+    }
+
+    const [users, setUsers] = useState([
+      { name: "Pat", balance: 500 },
+      { name: "Alex", balance: -250 }
+    ])
+
+    const [newUserName, setNewUserName] = useState("");
+
+    const [newUserBalance, setNewUserBalance] = useState(0);
+
   return (
-    <main className="flex min-h-screen items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
       <h1 className="text-5xl font-bold">
         Hating Kapatid 💸
       </h1>
@@ -29,12 +39,33 @@ export default function Home() {
       </p>
 
       {users.map((user) => (
-        <UserCard
-          key={user.name}
-          name={user.name}
-          balance={user.balance}
-        />
+        <p key={user.name}>
+          {user.name} - ₱{user.balance}
+        </p>
       ))}
+
+      <input
+          type="text"
+          placeholder="Enter name"
+          value={newUserName}
+          onChange={(e) => setNewUserName(e.target.value)}
+          className="border rounded px-3 py-2"
+      />
+
+      <input
+          type="number"
+          placeholder="Enter balance"
+          value={newUserBalance}
+          onChange={(e) => setNewUserBalance(Number(e.target.value))}
+          className="border rounded px-3 py-2"
+      />
+
+      <button
+          onClick = {addUser}
+          className = "bg-blue-500 text-white px-4 py-2 rounded"
+      >
+        Add User
+      </button>
 
     </main>
   );
